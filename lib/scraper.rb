56a47ea6843a7a908acdf783aa99d71bcf42ab40
cassymarie <cassy2205@gmail.com>
2020-11-05 09:04:34 -0700

    commit tree ade242ac128f9441fb41b3bc716ebf6a91adbbd4

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+courses = doc.css(".inlineMobileLeft-2Yo002 .title-oE5vT4")
+
+courses.each do |course|
+     puts course.text.strip
+end
+
+# binding.pry
+
